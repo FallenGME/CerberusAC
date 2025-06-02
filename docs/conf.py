f817ab39cdd717b9262c760bc76cfb2fe9c59aef
FallenGME <mrhello2227222@gmail.com>
2025-06-02 19:26:02 +0200

    t

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-project = 'MyProject'
-
-# Extensions you want to use
-extensions = ['sphinx.ext.autodoc']
-
-# Use the Read the Docs theme
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
--- docs/index.rst
+++
@@ -1,9 +0,0 @@
-Welcome to MyProject's documentation!
-======================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-   usage
-   api
